d94fd812c81552a3c92ad13fc1a484196fd10b9f
Harekasa <2459743667@qq.com>
2026-04-03 14:17:53 +0800

    修复脚本问题

--- scripts/raspi/ros2_startup.sh
+++ scripts/raspi/ros2_startup.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/zsh
+set -e
+
+# Some ROS/colcon setup scripts may read this variable in tracing paths.
+export AMENT_TRACE_SETUP_FILES=""
 
 export ROS_LOG_DIR=/home/harekasa/mygo_ws/log/startup
 mkdir -p "${ROS_LOG_DIR}"
 
-source /opt/ros/jazzy/setup.bash
-source /home/harekasa/mygo_ws/install/setup.bash
+source /opt/ros/jazzy/setup.zsh
+source /home/harekasa/mygo_ws/install/setup.zsh
 
 exec ros2 launch mygo_bringup bringup_on_raspi.launch.xml
